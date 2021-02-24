6066b702ace97344481384207f43ac7dc16b0a38
laertlucas <lucaslaert@hotmail.com>
2021-02-24 16:56:35 -0300

    Desafios Java

---
+++ DesafiosJava/src/Q3.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class Q3 {
+	public static void main(String[] args) {
+		int n1, n2, n3;
+
+		n1 = Integer.parseInt(JOptionPane.showInputDialog("Informe o primeiro número: "));
+		n2 = Integer.parseInt(JOptionPane.showInputDialog("Informe o segundo número: "));
+		n3 = Integer.parseInt(JOptionPane.showInputDialog("Informe o terceiro número: "));
+		
+		if ((n1 > n2) && (n1 > n3)) {
+			JOptionPane.showMessageDialog(null,"O maior número é o " + n1);
+		} else if ((n2 > n1) && (n2 > n3)) {
+			JOptionPane.showMessageDialog(null,"O maior número é o " + n2);
+		} else {
+		
+		JOptionPane.showMessageDialog(null,"O maior número é o " + n3);
+		}
+		
+		if ((n1 < n2) && (n1 < n3)) {
+			JOptionPane.showMessageDialog(null,"O menor número é o " + n1);
+		} else if ((n2 < n1) && (n2 < n3)) {
+			JOptionPane.showMessageDialog(null,"O menor número é o " + n2);
+		} else {
+		
+		JOptionPane.showMessageDialog(null,"O menor número é o " + n3);
+		}
+		
+}
+}
+
+/* Escreva um programa para calcular o maior e o menor de três números
+inteiros.
+*/
