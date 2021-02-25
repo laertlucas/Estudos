b0815774319680029fb30a6a656d93a9099bfd23
laertlucas <lucaslaert@hotmail.com>
2021-02-24 23:33:26 -0300

    Desafios Java

--- DesafiosJava/src/Q4.java
+++ DesafiosJava/src/Q4.java
@@ -1,31 +1,26 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Q4 {
 	public static void main(String[] args){
-		int i = 4;
-		int[] v = new int [i];
-		int m1 = 0;  
-		int m2 = 0;
-		int s;
-		int soma;
+	/*	int i = 3;
+		int[] v = new int[i];
 		
-		for (i = 1; i < v.length; i++) {
-			s = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe " + i + "º numero"));
-			if (s> m1) {
-				m1 = s;
-			}
-			if (s > m2) {
-				m2 = s;
-			}
-		}
+		for (i = 1; i < 3; i++) {
+			JOptionPane.showInputDialog("Insira "+ i + "º");
+			v[i] = 
+		}*/
 		
-		soma = m1 + m2;
-		
-		JOptionPane.showMessageDialog(null, soma);
-		
-	}
-}
+		Scanner input = new Scanner(System.in);
+	    int[] grade = new int[3];
 
+	    for (int i = 0; i < 3; i++) {
+	        System.out.println("Insert your grade: ");
+	        grade[i] = input.nextInt();
+	    }
+	    System.out.println(grade[1]);
+}
+}
 /*
  *Escreva um programa para calcular, dados três números inteiros, a soma
   dos dois maiores.
